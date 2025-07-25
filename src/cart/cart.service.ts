a2261257f7fa63f322f63b81838be6f3b8a83224
Muhammadali-fr <boburjonr934@gmail.com>
2025-07-25 22:40:26 +0500

    clean

--- src/cart/cart.service.ts
+++ src/cart/cart.service.ts
@@ -4,27 +4,4 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class CartService {
-
-    constructor(private prisma: PrismaService) { }
-
-    async getCart(userId: string) {
-        return this.prisma.cart.findUnique({
-            where: { userId },
-            include: { items: true }
-        })
-    }
-
-    async addToCart(userId: string, productId: string, quantity: number = 1) {
-        let cart = this.prisma.cart.findUnique({ where: { userId } });
-
-        if (!cart) {
-            cart = await this.prisma.cart.create({
-                data: {
-                    userId
-                }
-            })
-        }
-    }
-
-}
+export class CartService {}
