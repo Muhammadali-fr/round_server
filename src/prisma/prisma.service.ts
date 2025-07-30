14ad36780193485d6163d192f945437b9515633f
Muhammadali-fr <boburjonr934@gmail.com>
2025-07-30 19:13:37 +0500

    prisma service

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -6,7 +6,7 @@ import { PrismaClient } from '@prisma/client';
 export class PrismaService extends PrismaClient implements OnModuleInit {
     onModuleInit() {
         this.$connect()
-            .then(() => console.log('Prisma connected successfully'))
-            .catch((error) => console.error('Prisma connection error:', error));
+            .then(() => console.log('database connected successfully'))
+            .catch((error) => console.error('database connection error:', error));
     }
 }
