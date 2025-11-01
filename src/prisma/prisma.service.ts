794c362be97b319b51603b3c68479a281cdfa9c4
Muhammadali <boburjonr934@gmail.com>
2025-11-01 09:35:38 +0500

    prisma client

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from 'generated/prisma';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit { 
+export class PrismaService extends PrismaClient implements OnModuleInit {
     onModuleInit() {
         this.$connect()
-        .then(() => console.log("database connected"))
-        .catch((err) => console.log(err))
+            .then(() => console.log("database connected"))
+            .catch((err) => console.log(err))
     }
 }
