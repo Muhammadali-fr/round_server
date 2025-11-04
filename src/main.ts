45263bcc098ba96c15e375856837007e0c9881ab
Muhammadali <boburjonr934@gmail.com>
2025-11-04 08:25:49 +0500

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,12 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe())
 
   app.enableCors({
-    origin: ["http://localhost:3000", 'http://192.168.1.117:3000', 'http://localhost:5173', "https://round-server-3.onrender.com"],
+    origin: ["http://localhost:3000", 'http://192.168.1.117:3000', 'http://localhost:5173'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization']
   });
 
   await app.listen(process.env.PORT ?? 8000);
-}
+};
 bootstrap();
