5a48738357f6a3d03af507676424ce817e4b8745
Muhammadali <boburjonr934@gmail.com>
2025-11-23 09:03:57 +0500

    fixed prisma

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,12 +1,25 @@
-import { Injectable } from '@nestjs/common';
-import { OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-    onModuleInit() {
-        this.$connect()
-            .then(() => console.log("database connected"))
-            .catch((err) => console.log(err))
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL!,
+    });
+
+    super({
+      adapter,
+    });
+  }
+
+  async onModuleInit() {
+    try {
+      await this.$connect();
+      console.log("Database connected");
+    } catch (e) {
+      console.error("Database connection error:", e);
     }
+  }
 }
